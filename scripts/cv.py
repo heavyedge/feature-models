daf18393c628740b39f9a85b1e2c4dd5d47b2592
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-05-18 17:42:39 +0900

    refact: use newer dataset
    Implementing CV

--- scripts/cv.py
+++ scripts/cv.py
@@ -4,10 +4,17 @@ import pathlib
 import gpqr
 import pandas as pd
 import torch
+from gpytorch.mlls import VariationalELBO
+from gpytorch_qr.likelihoods import MultitaskCenterGapQuantileGPLikelihood
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
 QUANTILES = torch.tensor([0.05, 0.25, 0.5, 0.75, 0.95])
+CENTER_QUANTILE_INDEX = 2
+NUM_LOWER_QUANTILES = 2
+NUM_LATENTS = 3
+NUM_LOWER_LATENTS = 1
+K = 5
 
 parser = argparse.ArgumentParser()
 parser.add_argument("X", type=pathlib.Path, help="Feature csv file.")
@@ -15,7 +22,6 @@ parser.add_argument("y", type=pathlib.Path, help="Target csv file.")
 parser.add_argument("--target", type=str, help="Target variable name.")
 parser.add_argument("--model", type=str, help="Model class prefix.")
 parser.add_argument("--n-epochs", type=int, help="Number of training epochs.")
-parser.add_argument("--n-trials", type=int, help="Number of optimization trials.")
 parser.add_argument("-o", "--out", type=pathlib.Path, help="Output file.")
 parser.add_argument("--device", choices=["cpu", "cuda"], help="Device to train on")
 args = parser.parse_args()
@@ -33,7 +39,6 @@ if model_cls is None:
 X = torch.tensor(pd.read_csv(args.X).values).float()
 y = torch.tensor(pd.read_csv(args.y)[args.target].values).float()
 
-K = 5
 kf = KFold(n_splits=K, shuffle=True, random_state=42)
 x_train_list, y_train_list, x_test_list, y_test_list = [], [], [], []
 x_scales, x_means = [], []
@@ -53,3 +58,34 @@ x_test_cv = torch.stack(x_test_list).to(device)
 y_test_cv = torch.stack(y_test_list).to(device)
 x_scales = torch.stack(x_scales).to(device)
 x_means = torch.stack(x_means).to(device)
+
+inducing_points = X.clone().detach()
+if args.model == "CgLmcMtgpqr":
+    model = model_cls(
+        inducing_points=inducing_points,
+        num_quantiles=len(QUANTILES),
+        num_lower_quantiles=NUM_LOWER_QUANTILES,
+        num_latents=NUM_LATENTS,
+        num_lower_latents=NUM_LOWER_LATENTS,
+        X_scale=x_scales,
+        X_mean=x_means,
+        batch_shape=(K,),
+    ).to(device)
+    likelihood = MultitaskCenterGapQuantileGPLikelihood(
+        QUANTILES.reshape(K, -1),
+        CENTER_QUANTILE_INDEX,
+        torch.zeros((K, len(QUANTILES))),
+        learn_scales=True,
+    ).to(device)
+
+mll = VariationalELBO(likelihood, model, num_data=y_train_cv.shape[1])
+optimizer = torch.optim.Adam(
+    list(model.parameters()) + list(likelihood.parameters()),
+    lr=0.001,
+)
+
+for _ in range(1):
+    model.train()
+    likelihood.train()
+
+    output = model(x_train_cv)
