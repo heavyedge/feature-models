fix: scaling

scripts/plot-quantiles.py
@@ -32,7 +32,8 @@ data = pd.concat([X, y], axis=1)
 
 model_cls_name = f"{args.model}_{args.target}"
 model_cls = getattr(model_module, model_cls_name, None)
-model = load_model(model_cls, args.checkpoint, device=device).eval()
+model, scaler = load_model(model_cls, args.checkpoint, device=device)
+model.eval()
 
 # Plot
 
@@ -73,8 +74,9 @@ for ax, ((cos_theta,), df) in zip(axes, groups):
             ],
             axis=-1,
         )
+        X_pred_scaled = scaler.transform(X_pred)
         with torch.no_grad():
-            quantiles = model(torch.tensor(X_pred).float().to(device))
+            quantiles = model(torch.tensor(X_pred_scaled).float().to(device))
         q_low, q_high = quantiles.T[[0, -1], ...].cpu().numpy()
         ax.fill_between(
             Rgt_pred,