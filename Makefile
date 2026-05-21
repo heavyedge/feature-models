9ac1c66736e5bfd2a028802702ce7a6f9507937a
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-05-21 19:00:23 +0900

    feat: add deterministic quality window
    Modify X

--- Makefile
+++ Makefile
@@ -33,7 +33,7 @@ _temp/Dataset.csv: scripts/filter-dataset.py _data/Dataset.csv
 	python3 $^ -o $@
 
 _temp/X.csv: _temp/Dataset.csv
-	python3 -c "import pandas as pd; import numpy as np; df = pd.read_csv('$<')[['Gap_to_thickness_ratio', 'Capillary_number', 'Contact_angle']]; df['Cos_theta'] = np.cos(np.radians(df['Contact_angle'])); df.drop('Contact_angle', axis=1).to_csv('$@', index=False)"
+	python3 -c "import pandas as pd; import numpy as np; df = pd.read_csv('$<')[['Slurry', 'Gap_to_thickness_ratio', 'Capillary_number', 'Contact_angle']]; df['Cos_theta'] = np.cos(np.radians(df['Contact_angle'])); df.drop('Contact_angle', axis=1).to_csv('$@', index=False)"
 
 _temp/y.csv: _temp/Dataset.csv
 	python3 -c "import pandas as pd; pd.read_csv('$<')[['H', 'phi']].to_csv('$@', index=False)"
@@ -60,4 +60,4 @@ _temp/window.npy: _temp/window.H.npy _temp/window.phi.npy
 	python3 -c "import numpy as np; qw = np.all([np.load(f) for f in '$^'.split(' ')], axis=0).flatten(); np.save('$@', qw)"
 
 model/QW.SVC.pkl: scripts/train-svc.py _temp/X.csv _temp/window.npy
-	python3 $^ --n-trials 100 -o $@
+	python3 $^ -o $@
--- scripts/train-svc.py
+++ scripts/train-svc.py
@@ -5,7 +5,7 @@ import pickle
 import numpy as np
 import optuna
 import pandas as pd
-from kernel import AnisotropicMatern52
+from kernels import AnisotropicMatern52 as kernel
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
@@ -14,11 +14,13 @@ from sklearn.svm import SVC
 parser = argparse.ArgumentParser()
 parser.add_argument("X", type=pathlib.Path, help="Observed X csv file.")
 parser.add_argument("window", type=pathlib.Path, help="Quality window npy file.")
-parser.add_argument("--n-trials", type=int, help="Number of Optuna trials.")
 parser.add_argument("-o", "--out", type=pathlib.Path, help="Output pkl file.")
+parser.add_argument(
+    "--n-trials", type=int, default=100, help="Number of Optuna trials per slurry."
+)
 args = parser.parse_args()
 
-X = pd.read_csv(args.X).to_numpy()
+X = pd.read_csv(args.X).drop(columns="Slurry").to_numpy()
 W = np.load(args.window)
 
 optuna.logging.set_verbosity(optuna.logging.WARNING)
@@ -27,7 +29,7 @@ optuna.logging.set_verbosity(optuna.logging.WARNING)
 def objective(trial, x, y):
     n_features = x.shape[1]
     C = trial.suggest_float("C", 1e-3, 1e3, log=True)
-    gammas = AnisotropicMatern52(
+    gammas = kernel(
         [
             trial.suggest_float(f"gamma_{j}", 1e-5, 1e2, log=True)
             for j in range(n_features)
@@ -52,7 +54,7 @@ study.optimize(
 )
 
 best = study.best_params
-best_gammas = AnisotropicMatern52([best[f"gamma_{j}"] for j in range(X.shape[1])])
+best_gammas = kernel([best[f"gamma_{j}"] for j in range(X.shape[1])])
 model = Pipeline(
     [
         ("scaler", MinMaxScaler()),
