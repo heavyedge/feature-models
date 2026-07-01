7c2f9402efb485d69887bd45db00de684649bb81
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-07-01 22:59:30 +0900

    refact: gpr
    Add scale.py

--- scripts/model/__init__.py
+++ scripts/model/__init__.py
@@ -3,3 +3,4 @@ from .gpqr_other import *  # NOQA
 from .gpr import *  # NOQA
 from .gpr_other import *  # NOQA
 from .prior import *  # NOQA
+from .scale import *  # NOQA
---
+++ scripts/model/scale.py
@@ -0,0 +1,29 @@
+import torch
+
+__all__ = [
+    "Scaler",
+]
+
+
+class Scaler(torch.nn.Module):
+    """Min-max-scaling.
+
+    Parameters
+    ----------
+    X_scale, X_min: torch.Tensor in shape (*B, D)
+        Values from sklearn.preprocessing.MinMaxScaler.
+    """
+
+    def __init__(self, X_scale, X_min):
+        super().__init__()
+        self.register_buffer("X_scale", X_scale)
+        self.register_buffer("X_min", X_min)
+
+    def forward(self, x):
+        # x: (*B, N, D)
+        # X_scale, X_min: (*B, D)
+        # BELOW IS CORRECT FOR MinMaxScaler (different from StandardScaler)
+        X_min = self.X_min.unsqueeze(-2)
+        X_scale = self.X_scale.unsqueeze(-2)
+        x_scaled = x * X_scale + X_min
+        return x_scaled.view_as(x)
