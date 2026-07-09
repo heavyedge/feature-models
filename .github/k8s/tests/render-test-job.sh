7e788a60a4af881a09a96f46a94b548af113fdbd
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-07-09 11:22:11 +0900

    maint: add ci test for k8s
    Adding tests

--- .github/k8s/tests/render-test-job.sh
+++ .github/k8s/tests/render-test-job.sh
@@ -3,6 +3,7 @@ set -eu
 
 script_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 k8s_dir="$(CDPATH= cd -- "${script_dir}/.." && pwd)"
+override_dir="${1:-${script_dir}/containers}"
 tmp_dir="$(mktemp -d "${TMPDIR:-/tmp}/heavyedge-k8s-test.XXXXXX")"
 
 cleanup() {
@@ -12,6 +13,6 @@ cleanup() {
 trap cleanup EXIT
 
 cp "${k8s_dir}"/containers/*.yaml "${tmp_dir}/"
-cp "${script_dir}/containers/build.yaml" "${tmp_dir}/build.yaml"
+cp "${override_dir}"/*.yaml "${tmp_dir}/"
 
 sh "${k8s_dir}/render-job.sh" "${k8s_dir}/job.yaml" "${tmp_dir}"
