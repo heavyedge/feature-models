a05c9f4baa8796d639ec4c56d0d427f0b2e26c32
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-07-09 20:39:59 +0900

    feat: email
    Modify email

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,11 +2,7 @@
 
 set -eu
 
-python .github/k8s/send-email.py --status "Started" || true
-
 GITHUB_APP_TOKEN="$(sh .github/scripts/app-token.sh)"
 export GITHUB_APP_TOKEN
 
 sh .github/scripts/dispatch-cleanup.sh
-
-python .github/k8s/send-email.py --status "Completed" || true
