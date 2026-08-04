5a1a2056640165d54ccb2840e0e457bfafe9d08a
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-04 23:52:41 +0900

    feat: implement v1
    Modify data preprocesing

--- make/v0.mk
+++ make/v0.mk
@@ -47,11 +47,39 @@ test-v0: $(MODEL_FILES)
 
 # Data
 
-_temp/v0/X.csv: scripts/v0/data/write-X.py _data/v1/dimless.csv
+_temp/v0/dimless.csv: $(wildcard _data/v1/dimless/mean_profiles/dataset*.csv)
+	mkdir -p $(@D)
+	python3 -c '
+	from pathlib import Path
+	import pandas as pd
+
+	pvs = sorted([Path(f) for f in "$^".split(" ")])
+	df = pd.concat(
+		[pd.read_csv(f, dtype=str).assign(name=lambda df: f"{f.stem}/" + df["name"]) for f in pvs],
+		ignore_index=True,
+	)
+	df.to_csv("$@", index=False)
+	'
+
+_temp/v0/shape_features.csv: $(wildcard _data/v1/shape_features/mean_profiles/dataset*.csv)
+	mkdir -p $(@D)
+	python3 -c '
+	from pathlib import Path
+	import pandas as pd
+
+	pvs = sorted([Path(f) for f in "$^".split(" ")])
+	df = pd.concat(
+		[pd.read_csv(f, dtype=str).assign(name=lambda df: f"{f.stem}/" + df["name"]) for f in pvs],
+		ignore_index=True,
+	)
+	df.to_csv("$@", index=False)
+	'
+
+_temp/v0/X.csv: scripts/v0/data/write-X.py _temp/v0/dimless.csv
 	mkdir -p $(@D)
 	python3 $^ -o $@
 
-_temp/v0/y.csv: scripts/v0/data/write-y.py _temp/v0/X.csv _data/v1/shape_features/mean_profiles.csv
+_temp/v0/y.csv: scripts/v0/data/write-y.py _temp/v0/X.csv _temp/v0/shape_features.csv
 	python3 $^ -o $@
 
 _temp/v0/Xtrain.csv: _temp/v0/X.csv
--- setup.sh
+++ setup.sh
@@ -29,41 +29,54 @@ wait "$features_pid"
 
 # Postprocess data
 
+## Expand process_variables
+
+mkdir -p _data/v1/process_variables/mean_profiles
 python3 -c '
 from pathlib import Path
 import pandas as pd
 
-pvs = sorted(Path("_data/v1/process_variables/").glob("*.csv"))
-df = pd.concat(
-    [pd.read_csv(f, dtype=str).assign(name=lambda df: f"{f.stem}/" + df["name"]) for f in pvs],
-    ignore_index=True,
-)
-df.to_csv("_data/v1/process_variables.csv", index=False)
+pvs = sorted(Path("_data/v1/process_variables/").glob("dataset*.csv"))
+features = sorted(Path("_data/v1/shape_features/mean_profiles/").glob("dataset*.csv"))
+
+for pv, feat in zip(pvs, features):
+    pv_df = pd.read_csv(pv, dtype=str)
+    feature_names = pd.read_csv(feat, dtype=str)["name"]
+
+    expanded_pv = pv_df.set_index("name").reindex(feature_names).reset_index()
+    out = Path("_data/v1/process_variables/mean_profiles") / pv.name
+    expanded_pv.to_csv(out, index=False)
 '
+
+mkdir -p _data/v1/process_variables/profiles
+python3 -c '
+from pathlib import Path
+import pandas as pd
+
+pvs = sorted(Path("_data/v1/process_variables/").glob("dataset*.csv"))
+features = sorted(Path("_data/v1/shape_features/profiles/").glob("dataset*.csv"))
+
+for pv, feat in zip(pvs, features):
+    pv_df = pd.read_csv(pv, dtype=str)
+    feature_names = pd.read_csv(feat, dtype=str)["name"]
+
+    expanded_pv = pv_df.set_index("name").reindex(feature_names).reset_index()
+    out = Path("_data/v1/process_variables/profiles") / pv.name
+    expanded_pv.to_csv(out, index=False)
+'
+
+## Write dimensionless data
 
 uv pip install --system -r libs/profile-dataset/requirements.txt -r libs/profile-dataset/examples/requirements.txt
-papermill libs/profile-dataset/examples/v1/dimless.ipynb - -p pv_path _data/v1/process_variables.csv -p metadata_path _data/v1/datapackage.json -p out_path "_data/v1/dimless.csv" > /dev/null 2>&1
 
-python3 -c '
-from pathlib import Path
-import pandas as pd
+mkdir -p _data/v1/dimless/mean_profiles
+for f in _data/v1/process_variables/mean_profiles/*.csv; do
+    out="_data/v1/dimless/mean_profiles/$(basename "$f")"
+    papermill libs/profile-dataset/examples/v1/dimless.ipynb - -p pv_path "$f" -p metadata_path _data/v1/datapackage.json -p out_path "$out" > /dev/null 2>&1
+done
 
-pvs = sorted(Path("_data/v1/shape_features/mean_profiles/").glob("*.csv"))
-df = pd.concat(
-    [pd.read_csv(f, dtype=str).assign(name=lambda df: f"{f.stem}/" + df["name"]) for f in pvs],
-    ignore_index=True,
-)
-df.to_csv("_data/v1/shape_features/mean_profiles.csv", index=False)
-'
-
-python3 -c '
-from pathlib import Path
-import pandas as pd
-
-pvs = sorted(Path("_data/v1/shape_features/profiles/").glob("*.csv"))
-df = pd.concat(
-    [pd.read_csv(f, dtype=str).assign(name=lambda df: f"{f.stem}/" + df["name"]) for f in pvs],
-    ignore_index=True,
-)
-df.to_csv("_data/v1/shape_features/profiles.csv", index=False)
-'
+mkdir -p _data/v1/dimless/profiles
+for f in _data/v1/process_variables/profiles/*.csv; do
+    out="_data/v1/dimless/profiles/$(basename "$f")"
+    papermill libs/profile-dataset/examples/v1/dimless.ipynb - -p pv_path "$f" -p metadata_path _data/v1/datapackage.json -p out_path "$out" > /dev/null 2>&1
+done
