749cb1a5e985771b01a742392a10d6aa9d06eefc
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-14 16:10:39 +0900

    refact: redesign v0
    Add more test

--- tests/v0/test_predict.py
+++ tests/v0/test_predict.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 
-def test_predict_priormean_H(models_path, Xtest_path, tmp_path):
+def test_predict_priormean(models_path, Xtest_path, tmp_path):
     output_path = tmp_path / "predicted_H.csv"
 
     subprocess.run(
@@ -13,6 +13,24 @@ def test_predict_priormean_H(models_path, Xtest_path, tmp_path):
             str(Xtest_path),
             "--target",
             "H",
+            "--out",
+            str(output_path),
+        ],
+        check=True,
+        cwd=models_path,
+    )
+    assert output_path.exists()
+
+    output_path = tmp_path / "predicted_phi.csv"
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "feature_models.predict-prior_mean",
+            str(Xtest_path),
+            "--target",
+            "phi",
             "--out",
             str(output_path),
         ],
