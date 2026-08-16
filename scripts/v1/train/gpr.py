deff96d2e3c9309310cfcb3acf2faa4b5346af89
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-16 22:56:00 +0900

    feat: v1 GPR
    Try fix ci

--- scripts/v1/train/gpr.py
+++ scripts/v1/train/gpr.py
@@ -4,15 +4,15 @@ import pathlib
 import sys
 
 import gpytorch
-import models.v1.feature_models.gpr as model_module
-import models.v1.feature_models.load as load_module
-import models.v1.feature_models.scale as scaler_module
 import numpy as np
 import optuna
 import torch
 from gpytorch.mlls import VariationalELBO
 from models.v1.feature_models.likelihoods import GaussianLikelihood
 
+from models.v1.feature_models import gpr as model_module
+from models.v1.feature_models import load as load_module
+from models.v1.feature_models import scale as scaler_module
 from scripts.v0.train.batch import load_batched_arrays
 from scripts.v0.train.inducing import unique_inducing_points_per_fold
 from scripts.v1.train.save import save_gpr
