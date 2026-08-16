7b2aceba7d86a23e9514fe9b5046a9cfabae7323
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-16 14:39:43 +0900

    refact: v1
    Modifying v1

--- make/v1.mk
+++ make/v1.mk
@@ -6,7 +6,6 @@ NUM_LATENTS := 3
 
 N_LIKELIHOOD_SAMPLES := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),4,64)
 N_EPOCHS := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),1,10000)
-N_FOLDS := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),2,10)
 N_GRID_1 := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),2,200)
 N_GRID_2 := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),2,10)
 N_TRIALS := $(if $(filter 1,$(HEAVYEDGE_TEST_MODE)),1,100)
@@ -64,16 +63,22 @@ _temp/v1/shape_features.csv: $(wildcard _data/v1/shape_features/profiles/dataset
 	df.to_csv("$@", index=False)
 	'
 
-_temp/v1/X.csv: scripts/v1/data/write-X.py _temp/v1/dimless.csv
-	python3 $^ --split-ratio 0.8 0.1 0.1 --random-state=42 -o $@
-
-_temp/v1/y.csv: scripts/v1/data/write-y.py _temp/v1/X.csv _temp/v1/shape_features.csv
+_temp/v1/X.csv: scripts/v0/data/write-X.py _temp/v1/dimless.csv
 	python3 $^ -o $@
 
+_temp/v1/y.csv: scripts/v0/data/write-y.py _temp/v1/X.csv _temp/v1/shape_features.csv
+	python3 $^ --index-col 0 1 2 -o $@
+
+_temp/v1/Xsplit.csv: scripts/v0/data/split-X.py _temp/v1/X.csv
+	python3 $^ --split-ratio 0.8 0.1 0.1 --num-folds 1 --random-state=42 -o $@
+
+_temp/v1/ysplit.csv: scripts/v0/data/write-y.py _temp/v1/Xsplit.csv _temp/v1/shape_features.csv
+	python3 $^ --index-col 0 1 2 3 4 -o $@
+
 define SPLIT_v1
-_temp/v1/X$(1).csv: _temp/v1/X.csv
-	python3 -c "import pandas as pd; df = pd.read_csv('$$<'); mask = df['split'] == '$(1)'; df.loc[mask, ['gap_to_thickness_ratio', 'capillary_number', 'cosine_of_contact_angle']].to_csv('$$@', index=False)"
-_temp/v1/y$(1).csv: _temp/v1/y.csv
-	python3 -c "import pandas as pd; df = pd.read_csv('$$<'); mask = df['split'] == '$(1)'; df.loc[mask, ['H', 'phi']].to_csv('$$@', index=False)"
+_temp/v1/X$(1).csv: _temp/v1/Xsplit.csv
+	python3 -c "import pandas as pd; df = pd.read_csv('$$<'); mask = df['split'] == '$(1)'; df.loc[mask, ['fold', 'gap_to_thickness_ratio', 'capillary_number', 'cosine_of_contact_angle']].to_csv('$$@', index=False)"
+_temp/v1/y$(1).csv: _temp/v1/ysplit.csv
+	python3 -c "import pandas as pd; df = pd.read_csv('$$<'); mask = df['split'] == '$(1)'; df.loc[mask, ['fold', 'H', 'phi']].to_csv('$$@', index=False)"
 endef
 $(foreach split,train val test,$(eval $(call SPLIT_v1,$(split))))
