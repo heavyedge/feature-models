0c7741426dd91ac25cbb6c3da344bc21b4793566
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-17 02:26:36 +0900

    maint: fix k8s/build.sh

--- .github/k8s/build.sh
+++ .github/k8s/build.sh
@@ -3,7 +3,7 @@ export MAKE_JOBS="$(python3 scripts/cuda-preflight.py --print-count)"
 python3 scripts/cuda-preflight.py
 
 if [ "${GITHUB_EVENT_NAME}" = "release" ]; then
-    HEAVYEDGE_TEST_MODE=0 make -j "$MAKE_JOBS" "models-${MAJOR_VERSION} examples-${MAJOR_VERSION}"
+    HEAVYEDGE_TEST_MODE=0 make -j "$MAKE_JOBS" models-${MAJOR_VERSION} examples-${MAJOR_VERSION}
 else
     HEAVYEDGE_TEST_MODE=1 make -j "$MAKE_JOBS" models tests examples
 fi
